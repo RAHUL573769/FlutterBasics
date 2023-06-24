f3d209214528df31cfefc307028b76d9b74186e4
Rahul Rudra <rahulrudra665@gmail.com>
2023-06-24 08:37:44 +0600

    styles commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Colors.purpleAccent, Colors.purple]),
+          ),
+          child: const Center(
+            child: Text("Hello World", style: TextStyle(fontSize: 34)),
+          ),
         ),
       ),
     ),
