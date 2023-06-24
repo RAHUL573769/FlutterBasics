0c0e7cedc821c9f774de42acd28183d72f3a4562
Rahul Rudra <rahulrudra665@gmail.com>
2023-06-24 08:40:07 +0600

    gragient commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient:
-                LinearGradient(colors: [Colors.purpleAccent, Colors.purple]),
-          ),
-          child: const Center(
-            child: Text("Hello World", style: TextStyle(fontSize: 34)),
-          ),
-        ),
+        body: GradientColor(),
       ),
     ),
   );
+}
+
+class GradientColor extends StatelessWidget {
+  const GradientColor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [Colors.purpleAccent, Colors.purple]),
+      ),
+      child: const Center(
+        child: Text("Hello World", style: TextStyle(fontSize: 34)),
+      ),
+    );
+  }
 }
